7cf428fc68204ea7b2e6d08b4981e8087792519c
lucasew <lucas59356@gmail.com>
2026-08-24 12:50:26 -0300

    revert: do not store LLVM pointers as uintptr
    Broke rhai/fn and seed 42 with a nil deref on every OS.

--- expression.go
+++ expression.go
@@ -237,24 +237,10 @@ func wholeVarAccess(v value.Value, elem types.Type) bool {
 	return types.Equal(g.ContentType, elem)
 }
 
-// llvmPtrBits is an LLVM pointer that may hold a C heap address or a
-// dangling alloca. Store it as uintptr so the write barrier does not
-// mark a freed Go object (Darwin seed 42).
-func llvmPtrBits(elem types.Type) bool {
-	_, ok := elem.(*types.PointerType)
-	return ok && !isTaggedPointerType(elem)
-}
-
 func overlayMem(addr *jen.Statement, addrTy, elem types.Type) (*jen.Statement, error) {
-	var t *jen.Statement
-	if llvmPtrBits(elem) {
-		t = jen.Uintptr()
-	} else {
-		var err error
-		t, err = TypeSpec(elem)
-		if err != nil {
-			return nil, err
-		}
+	t, err := TypeSpec(elem)
+	if err != nil {
+		return nil, err
 	}
 	if isTaggedPointerType(addrTy) {
 		addr = emitUP(addr)
@@ -267,14 +253,7 @@ func typedLoad(src expr, srcVal value.Value, elem types.Type) (*jen.Statement, e
 	// not the object as a Go slice or as a pointer value.
 	if g, ok := srcVal.(*ir.Global); ok && isStdStream(VariableName(g)) {
 		if _, ok := elem.(*types.PointerType); ok {
-			slot, err := overlayMem(src.code, srcVal.Type(), elem)
-			if err != nil {
-				return nil, err
-			}
-			if llvmPtrBits(elem) {
-				return emitUP(slot), nil
-			}
-			return slot, nil
+			return overlayMem(src.code, srcVal.Type(), elem)
 		}
 	}
 	if src.base != nil && wholeVarAccess(srcVal, elem) {
@@ -295,18 +274,12 @@ func typedLoad(src expr, srcVal value.Value, elem types.Type) (*jen.Statement, e
 	if err != nil {
 		return nil, err
 	}
-	if llvmPtrBits(elem) {
-		return emitUP(slot), nil
-	}
 	return slot, nil
 }
 
 func typedStore(dst expr, dstVal value.Value, elem types.Type, src jen.Code) (*jen.Statement, error) {
 	if dst.base != nil && wholeVarAccess(dstVal, elem) {
 		return dst.store(src), nil
-	}
-	if llvmPtrBits(elem) {
-		src = jen.Uintptr().Call(emitUP(src))
 	}
 	slot, err := overlayMem(dst.code, dstVal.Type(), elem)
 	if err != nil {
